a186bb3633a19ab62a0fac97a69cfd72cb8b3496
Borys Koreniak <koreniak.borys@gmail.com>
2023-04-12 14:12:28 +0300

    Виконав всі завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,16 @@
 import Player from '@vimeo/player';
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', throttle(() => {
+player.on('timeupdate', throttle(pausedTime, 1000));
+
+function pausedTime () {
     player.getCurrentTime().then(function(seconds) {
         localStorage.setItem("videoplayer-current-time", seconds);
     });
-}, 1000));
+};
 
 const currentTime = localStorage.getItem("videoplayer-current-time");
 console.log(currentTime);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,36 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+let formData = {};
+
 const feedbackForm = document.querySelector('.feedback-form');
-feedbackForm.addEventListener('input', throttle(changeInput, 500));
+feedbackForm.addEventListener('input', throttle(OnChangeInput, 1000));
+feedbackForm.addEventListener('submit', onFormSubmit);
 
-function changeInput(event) {
-    const currentValue = {
-        email: event.currentTarget.email.value,
-        message: event.currentTarget.message.value
-    };
-    console.log(event);
+populateForm();
 
-    localStorage.setItem("feedback-form-state", currentValue);
+function OnChangeInput(e) {
+    e.preventDefault();
+    
+    formData[e.target.name] = e.target.value;
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
-const localCurrentValue = localStorage.getItem("feedback-form-state");
+function onFormSubmit(e) {
+    e.preventDefault();
+    console.log(formData);
+
+    e.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+};
+
+function populateForm() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+
+    if (savedMessage) {
+        formData = JSON.parse(savedMessage)
+        feedbackForm.email.value = formData.email;
+        feedbackForm.message.value = formData.message;
+    };
+};
